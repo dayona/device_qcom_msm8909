2f32f030e064e7ff6d40eb21b441c77954660c50
Sreehari Vaddi <cshvadd@codeaurora.org>
2015-05-15 11:33:41 +0530

    msm8909: Add conf file to enable sensors multi hal feature
    Sensors multi hal reads the conf file and loads the sensor
    hal libraries mentioned to support sensors multi hal feature.
    
    Change-Id: Ia09ab4f3c68088d5cd5ec889c7704f7d0c2d0299

--- msm8909.mk
+++ msm8909.mk
@@ -149,3 +149,7 @@ PRODUCT_LOCALES += th_TH vi_VN tl_PH hi_IN ar_EG ru_RU tr_TR pt_BR bn_IN mr_IN t
 PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res-overlay \
         $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
         $(PRODUCT_PACKAGE_OVERLAYS)
+
+# Sensor HAL conf file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8909/sensors/hals.conf:system/etc/sensors/hals.conf
